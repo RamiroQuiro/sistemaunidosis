64fc9529cea8ff0741b160c4f43c0b0165af6fd3
ramirochangomoreno <rama.exe.13@gmail.com>
2023-06-09 15:04:31 -0300

    crando tabla cabecera

--- src/app/dashboard/pacientes/ContenedorPacientes.jsx
+++ src/app/dashboard/pacientes/ContenedorPacientes.jsx
@@ -16,24 +16,24 @@ export default function ContenedorPacientes({}) {
         className="flex bg-gray-200 items-center mx-auto justify-between p-1 w-full"
       >
         <div className="flex flex-col items-center justify-between p-1 gap-2 mx-auto">
-          <div className="container p-2 mx-auto sm:p-4 dark:text-gray-100">
+          <div className="container p-2 mx-auto sm:p-4 ">
             <div className="overflow-x-auto">
               <table className="min-w-full text-xs">
                 <thead className="dark:bg-gray-700">
                   <tr className="text-left">
-                    <th className="p-3">Invoice #</th>
-                    <th className="p-3">Client</th>
-                    <th className="p-3">Issued</th>
-                    <th className="p-3">Due</th>
-                    <th className="p-3 text-right">Amount</th>
-                    <th className="p-3">Status</th>
+                    <th className="p-3">id Paciente</th>
+                    <th className="p-3">paciente</th>
+                    <th className="p-3">doctora</th>
+                    <th className="p-3">algo</th>
+                    <th className="p-3 text-right">nose</th>
+                    <th className="p-3">ir a</th>
                   </tr>
                 </thead>
                 <tbody>
                   {pacientes?.map((paciente, i) => (
                     <tr
                       key={data[paciente]?.idPaciente}
-                      className="border-b border-opacity-20 dark:border-gray-700 dark:bg-gray-900"
+                      className="border-b border-opacity-20 dark:border-gray-700"
                     >
                       <td className="p-3">
                         <p>{data[paciente].idPaciente}</p>
@@ -49,7 +49,7 @@ export default function ContenedorPacientes({}) {
                       <td className="p-3 text-right">
                       </td>
                       <td className="p-3 text-right">
-                        <span className="px-3 py-1 font-semibold rounded-md dark:bg-violet-400 dark:text-gray-900">
+                        <span className="px-3 py-1 font-semibold rounded-md dark:bg-violet-400 ">
                           <span>ir a paciente</span>
                         </span>
                       </td>
