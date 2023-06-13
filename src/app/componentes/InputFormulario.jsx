60fe4326d50e7e6ebe7f7d3edccec08753a3b149
ramirochangomoreno <rama.exe.13@gmail.com>
2023-06-13 14:56:32 -0300

    creando panel/sidebarr

--- src/app/componentes/InputFormulario.jsx
+++ src/app/componentes/InputFormulario.jsx
@@ -1,14 +1,52 @@
-"use client"
+"use client";
 
 export default function InputFormulario({
-    onChange,type,name,value,children
+  label,
+  onChange,
+  type,
+  name,
+  value,
+  children,
 }) {
+  if (label == "date") {
+    return (
+      <div
+        className={`flex flex-col text-sm items-start justify-between w-1/3 relative py-2 `}
+      >
+        <label htmlFor={name} className="text-xs ">
+          {children}
+          <input
+            onChange={onChange}
+            type={type}
+            name={name}
+            id={name}
+            value={value}
+            required={true}
+            className="peer w- py-1 pl-4 focus:bg-transparent  bg-transparent shadow-none border-0 border-b text-gray-700 border-gray-500 outline-none focus:outline-none relative z-10 valid:bg-transparent bg-"
+          />
+        </label>
+      </div>
+    );
+  }
   return (
-    <div className={`flex flex-col text-sm items-start justify-between w-full relative py-2 `}>
-       <input 
-       onChange={onChange}
-       type={type} name={name} id={name}   value={value} required={true}   className="peer w-full py-1 pl-4 focus:bg-transparent  bg-transparent shadow-none border-0 border-b text-gray-700 border-gray-500 outline-none focus:outline-none relative z-10 valid:bg-transparent bg-" />
-       <label htmlFor="email" className=" peer-focus:bg-primary-100 peer-focus:text-gray-700 text-sm peer-focus:p-1 peer-focus:rounded-sm peer-focus:-translate-y-4 peer-valid:bg-primary-100 peer-valid:text-gray-700  peer-valid:p-1 peer-valid:rounded-sm peer-valid:text-xs peer-focus:text-xs bg-transparent volid:bg-transparent focus:bg-transparent  peer-valid:-translate-y-4 duration-500 text-gray-700 absolute left-0 bottom-3 pl-4">{children}</label>
-     </div>
-  )
+    <div
+      className={`flex flex-col text-sm items-start justify-between w-full relative py-2 `}
+    >
+      <input
+        onChange={onChange}
+        type={type}
+        name={name}
+        id={name}
+        value={value}
+        required={true}
+        className="peer w-full py-1 pl-4 focus:bg-transparent  bg-transparent shadow-none border-0 border-b text-gray-700 border-gray-500 outline-none focus:outline-none relative z-10 valid:bg-transparent bg-"
+      />
+      <label
+        htmlFor={name}
+        className=" peer-focus:bg-primary-100 peer-focus:text-gray-700 text-sm peer-focus:p-1 peer-focus:rounded-sm peer-focus:-translate-y-4 peer-valid:bg-primary-100 peer-valid:text-gray-700  peer-valid:p-1 peer-valid:rounded-sm peer-valid:text-xs peer-focus:text-xs bg-transparent volid:bg-transparent focus:bg-transparent  peer-valid:-translate-y-4 duration-500 text-gray-700 absolute left-0 bottom-3 pl-4"
+      >
+        {children}
+      </label>
+    </div>
+  );
 }
--- src/app/componentes/sidebar/FormularioSidebar.jsx
+++ src/app/componentes/sidebar/FormularioSidebar.jsx
@@ -10,10 +10,67 @@ export default function FormularioSidebar() {
     const inputRegister = [
         {
           id: 1,
+          label:"",
           name: "kilogramos",
           type: "number",
           onChange: handleChange,
           children: "Peso de paciente",
+        },
+        {
+          id: 2,
+          label:"date",
+          name: "fechaPrep",
+          type: "date",
+          onChange: handleChange,
+          children: "Fecha de Prep",
+        },
+        {
+          id: 3,
+          label:"date",
+          name: "horaPrep",
+          type: "time",
+          onChange: handleChange,
+          children: "Hora de Prep",
+        },
+        {
+          id: 4,
+          label:"date",
+          name: "fechaAdm",
+          type: "date",
+          onChange: handleChange,
+          children: "Fecha de Adm",
+        },
+        {
+          id: 5,
+          name: "horaAdm",
+          label:"date",
+          type: "time",
+          onChange: handleChange,
+          children: "Hora de Adm",
+        },
+        {
+          id: 6,
+          name: "volumenDosis",
+          label:"",
+          type: "number",
+          onChange: handleChange,
+          children: "Volumen de Dosis",
+        },
+        {
+          id: 7,
+          name: "operadora",
+          label:"",
+          type: "text",
+          onChange: handleChange,
+          children: "Operadora",
+        },
+        {
+          id: 8,
+          name: "asistente",
+          label:"",
+          type: "text",
+          onChange: handleChange,
+          children: "Asistente",
         },
     ]
 
@@ -21,13 +78,14 @@ export default function FormularioSidebar() {
   return (
     <form
     action=""
-    className="flex flex-col w-11/12 items-center justify-center mx-auto my-5 bg-gray-200 rounded-lg p-2"
+    className="flex flex-col w-11/12 items-center justify-center mx-auto my-5 bg-gray-100 border rounded-lg p-2"
   >
     <h2 className="font-medium">Datos a colocar en Rotulo</h2>
-  
+  <div className='flex flex-wrap gap-x-1 items-center justify-between px-0.5'>
   {
      inputRegister?.map((label) => (
         <InputFormulario
+        label={label.label}
         key={label.id}
         onChange={label.handleChange}
         type={label.type}
@@ -37,7 +95,7 @@ export default function FormularioSidebar() {
         {label.children}
       </InputFormulario>
      ))
-  }
+  }</div>
    
   </form>
   )
